0102e30093f72a17e94f7db67d11153868a4706c
Baarbod <Baarbod@10.0.0.139>
2018-12-11 09:40:07 -0500

    Removed waitbar

--- modules/hemodynamics/setflowdirection.m
+++ modules/hemodynamics/setflowdirection.m
@@ -1,13 +1,10 @@
 function edgenod = setflowdirection(edgenod,negFlow)
 
 % reverse pressure nodes based on negative flow
-f = waitbar(0,"Reversing pressure nodes for negative flows...");
 for ineg = 1:length(negFlow)
     edgeneg = negFlow(ineg);
     pneg1 = edgenod(edgeneg,1);
     pneg2 = edgenod(edgeneg,2);
     edgenod(edgeneg,1) = pneg2;
     edgenod(edgeneg,2) = pneg1;
-    waitbar(ineg/length(negFlow),f)
 end
-close(f)
