358de56e8b379f2180eed76f454ea85707cf4e4b
Baarbod <Baarbod@10.0.0.139>
2018-12-11 09:39:42 -0500

    Removed unnecessary handle from plot. Removed some unused lines.

--- modules/hemodynamics/visualize.m
+++ modules/hemodynamics/visualize.m
@@ -1,13 +1,11 @@
 function visualize(G,variable,xyz,fignum,title,clabel,climits,d)
-% create graph with color map linearly mapped to variable
 
 %% ------- Figure Properties ------- %%
 figure(fignum);
 fig = gcf;
 fig.Color = 'w';
-% fig.Position = [120 213 991 648];
 
-h = plot(G,'XData',xyz(:,1),'YData',xyz(:,2),'ZData',xyz(:,3),...
+plot(G,'XData',xyz(:,1),'YData',xyz(:,2),'ZData',xyz(:,3),...
     'EdgeCData',variable,'LineWidth',d,'EdgeAlpha',1,...
     'MarkerSize',0.1,'NodeLabel',[]);
 
@@ -25,11 +23,7 @@ ax.ZGrid = 'off';
 ax.XAxis.Label.String = 'x (\mum)';
 ax.YAxis.Label.String = 'y (\mum)';
 ax.ZAxis.Label.String = 'z (\mum)';
-ax.Color = [0.4 0.4 0.4];
-% ax.ColorScale = 'log';
-
-%% ------- View Properties ------- %%
-% view([0 90])
+ax.Color = 'k';
 
 %% ------- Colormap Properties ------- %%
 c = colorbar;
@@ -43,4 +37,3 @@ c.FontName = 'Times New Roman';
 map = autumn;
 colormap(flipud(map)) % reverse the colormap
 axis equal
-% savefig('flow_graph')
