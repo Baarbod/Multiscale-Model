c6d9317d054afdde1cfef596fd2e553a62f457f0
Baarbod <Baarbod@10.0.0.139>
2018-12-11 09:42:22 -0500

    Removed waitbars

--- modules/hemodynamics/buildlinearsystem.m
+++ modules/hemodynamics/buildlinearsystem.m
@@ -3,24 +3,19 @@ function [A, b] = buildlinearsystem(G,J,nodnod,bcnodind,...
 
 dbstop if error
 
-
 nnod = numnodes(G);
 A = zeros(nnod,nnod); % linear system matrix A
 b = zeros(nnod,1); % linear system matrix b
 
 % setup [A] matrix of coefficients
-f = waitbar(0,'Setting up [A] matrix...');
 for inod = 1:nnod
     nodnear = nonzeros(nodnod(inod,:)); % nodes connected to inod
     edgenear = findedge(G,inod,nodnear); % edges connected to inod
     A(inod,nodnear) = J(edgenear);
     A(inod,inod) = -sum(A(inod,nodnear));
-    waitbar(inod/nnod,f)
 end
-close(f)
 
 % setup [b] for boundary nodes
-f = waitbar(0,'Setting up [b] for boundary nodes...');
 for ibc = 1:length(bcnodind)
     bcnod = bcnodind(ibc);
     if ismember(bcnod,inflow_ind)
@@ -34,6 +29,4 @@ for ibc = 1:length(bcnodind)
         b(row) = b(row) - bcval*A(row,bcnod);
         A(row,bcnod) = 0;
     end
-    waitbar(ibc/length(inflow_ind),f)
 end
-close(f)
